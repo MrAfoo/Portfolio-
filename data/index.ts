d7bc64cc6a3d94661be2a582b8070625adf3ae09
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2025-05-13 21:27:17 +0500

    changes

--- data/index.ts
+++ data/index.ts
@@ -1,171 +1,168 @@
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
-    { name: "Contact", link: "#contact" },
-  ];
-  
-  export const gridItems = [
-    {
-      id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
-      spareImg: "",
-    },
-    {
-      id: 2,
-      title: "I'm very flexible with time zone communications",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 3,
-      title: "My tech stack",
-      description: "I constantly try to improve",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-center",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 4,
-      title: "Tech enthusiast with a passion for development.",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
-    },
-  
-    {
-      id: 5,
-      title: "Currently building a interactive landing pages",
-      description: "The Inside Scoop",
-      className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-      titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
-    },
-    {
-      id: 6,
-      title: "Do you want to start a project together?",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-      img: "",
-      spareImg: "",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "3D Image Slider ",
-      des: "A 3D image slider that showcases images with smooth transitions and depth effects, creating an interactive and visually engaging experience.",
-      img: "/p1.svg",
-      iconLists: ["/c.svg" , "html.svg"],
-      link: "/3d-image-slider.vercel.app",
-    },
-    {
-      id: 2,
-      title: "Gaming Website",
-      des: "A gaming website featuring an immersive design, smooth animations, and interactive elements, providing an engaging experience for gamers.",
-      img: "/p2.svg",
-      iconLists: ["/re.svg", "js.svg", "/gsap.svg", "/tail.svg", "/vite.svg"],
-      link: "https://gamiing-website.vercel.app",
-    },
-    {
-      id: 3,
-      title: "Dynamic-Resume-Builder",
-      des: "A dynamic resume builder that allows users to create, customize, and generate professional resumes with ease.",
-      img: "/p3.svg",
-      iconLists: ["/js.svg", "/html.svg", "/c.svg"],
-      link: "/https://my-dynamic-resume-builder.vercel.app",
-    },
-    {
-      id: 4,
-      title: "E-commerce Website( Elite Mart )",
-      des: "Elite Mart - a sleek and user-friendly e-commerce website offering a seamless shopping experience with a modern design and smooth navigation",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "/https://elitee-mart.vercel.app",
-    },
-  ];
-  
-  export const testimonials = [
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-  ];
-  
-  export const workExperience = [
-    {
-      id: 1,
-      title: "Frontend Developer",
-      desc: "Developed and maintained web applications using React.js, improving UI/UX and performance.",
-      className: "md:col-span-2",
-      thumbnail: "/exp1.svg",
-    },
-    {
-      id: 2,
-      title: "Freelance Frontend Developer",
-      desc: "Designed and developed web applications, ensuring responsiveness and accessibility.",
-      className: "md:col-span-2",
-      thumbnail: "/exp2.svg",
-    },
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Testimonials", link: "#testimonials" },
+	{ name: "Contact", link: "#contact" },
 ];
 
-  
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-    },
-    {
-      id: 2,
-      img: "/twit.svg",
-    },
-    {
-      id: 3,
-      img: "/link.svg",
-    },
-  ];
+export const gridItems = [
+	{
+		id: 1,
+		title: "I prioritize client collaboration, fostering open communication ",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/b1.svg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	{
+		id: 5,
+		title: "Currently building a interactive landing pages",
+		description: "The Inside Scoop",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.svg",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "3D Image Slider ",
+		des: "A 3D image slider that showcases images with smooth transitions and depth effects, creating an interactive and visually engaging experience.",
+		img: "/pp1.jpg",
+		iconLists: ["/c.svg", "html.svg"],
+		link: "https://3d-image-slider.vercel.app",
+	},
+	{
+		id: 2,
+		title: "Gaming Website",
+		des: "A gaming website featuring an immersive design, smooth animations, and interactive elements, providing an engaging experience for gamers.",
+		img: "/pp4.jpg",
+		iconLists: ["/re.svg", "jscon.svg", "/gsap.svg", "/tail.svg", "/vite.svg"],
+		link: "https://gamiing-website.vercel.app",
+	},
+	{
+		id: 3,
+		title: "Dynamic-Resume-Builder",
+		des: "A dynamic resume builder that allows users to create, customize, and generate professional resumes with ease.",
+		img: "/pp2.jpg",
+		iconLists: ["/jscon.svg", "/html.svg", "/c.svg"],
+		link: "https://my-dynamic-resume-builder.vercel.app",
+	},
+	{
+		id: 4,
+		title: "E-commerce Website( Elite Mart )",
+		des: "Elite Mart - a sleek and user-friendly e-commerce website offering a seamless shopping experience with a modern design and smooth navigation",
+		img: "/pp3.jpg",
+		iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+		link: "https://elitee-mart.vercel.app",
+	},
+];
+
+export const testimonials = [
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+	{
+		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: "Michael Johnson",
+		title: "Director of AlphaStream Technologies",
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: "Frontend Developer",
+		desc: "Developed and maintained web applications using React.js, improving UI/UX and performance.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	{
+		id: 2,
+		title: "Freelance Frontend Developer",
+		desc: "Designed and developed web applications, ensuring responsiveness and accessibility.",
+		className: "md:col-span-2",
+		thumbnail: "/exp2.svg",
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/git.svg",
+		link: "https://github.com/MrAfoo",
+	},
+	{
+		id: 2,
+		img: "/twit.svg",
+		link: "https://x.com/Afoobhai",
+	},
+	{
+		id: 3,
+		img: "/link.svg",
+		link: "https://www.linkedin.com/in/affoo-bhai-37b929300/recent-activity/all/",
+	},
+];
